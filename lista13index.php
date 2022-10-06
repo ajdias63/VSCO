1a4fd953702713aaeb51f9bed17dd24ec3dd59dc
Anna Julia <112115923+ajdias63@users.noreply.github.com>
2022-10-06 16:58:40 -0300

    Add files via upload

---
+++ lista13index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- João Papo-de-Pescador, homem de bem, comprou um microcomputador
+para controlar o rendimento diário de seu trabalho. Toda vez que ele traz um
+peso de peixes maior que o estabelecido pelo regulamento de pesca do estado
+de São Paulo (50 quilos) deve pagar uma multa de R$ 4,00 por quilo
+excedente. João precisa que você faça um programa que leia a variável peso
+(peso de peixes) e calcule o excesso. Gravar na variável excesso a quantidade
+de quilos além do limite e na variável multa o valor da multa que João deverá
+pagar. Imprima os dados do script com as mensagens adequadas. -->
+
+<form action="ex13.php">
+        Qual é a quantidade de peixes: <br />
+        <input type="number" name="peixe" />
+        <br /> <br />
+        <input type="submit" value="Verificar" >
+     </form>
+
+
+</body>
+</html>
